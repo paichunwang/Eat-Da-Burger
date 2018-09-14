d54d550e3e019984ebd0234743ce0782b1ed03f2
TheMelonOne <paichun.wang@yahoo.com>
2018-09-14 17:39:22 -0500

    adding PORT for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 const exphbs = require('express-handlebars');
