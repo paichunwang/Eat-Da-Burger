0fe590a35be71154346468ecf4bfd4bfaf1cf5c8
TheMelonOne <paichun.wang@yahoo.com>
2018-09-14 16:38:45 -0500

    adding key.js and .env with example file for entering custom credentials

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log("Connected Key.js");
+
+require("dotenv").config({ path: "./.env" });
+
+exports.clearDB = {
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+};
